37a0683739fd7a31be913e4bf2b0c9429ef60ad3
Frans Brion <kamiofdeath@hotmail.it>
2016-06-22 10:34:59 +0200

    Added EnumExtension

---
+++ AppKit/AppKit/Extensions/EnumExtension.cs
@@ -0,0 +1,64 @@
+﻿namespace AdMaiora.AppKit.Extensions
+{
+    using System;
+    using System.Reflection;
+    using System.Collections.Generic;
+
+    public class FriendlyStringAttribute : Attribute
+    {
+        public FriendlyStringAttribute(string friendlyText)
+        {
+            this.FriendlyText = friendlyText;
+        }
+
+        public string FriendlyText
+        {
+            get;
+            private set;
+        }
+    }
+
+    public class FriendlyLocalizedStringAttribute : Attribute
+    {
+        public FriendlyLocalizedStringAttribute(string friendlyKey)
+        {
+            this.FriendlyKey = friendlyKey;
+        }
+
+        public string FriendlyKey
+        {
+            get;
+            private set;
+        }
+    }
+
+    public static class EnumExtension
+    {
+        public static string ToFriendlyString(this Enum value)
+        {
+            FieldInfo field = value.GetType().GetRuntimeField(value.ToString());
+            var attribs = new List<object>(field.GetCustomAttributes(typeof(FriendlyStringAttribute), true));
+            if (attribs.Count > 0)
+                return ((FriendlyStringAttribute)attribs[0]).FriendlyText;
+
+            return value.ToString();
+        }
+
+        public static string ToFriendlyLocalizedString(this Enum value, object localizer)
+        {
+            FieldInfo field = value.GetType().GetRuntimeField(value.ToString());
+            var attribs = new List<object>(field.GetCustomAttributes(typeof(FriendlyLocalizedStringAttribute), true));
+            if (attribs.Count > 0)
+            {
+                string key = ((FriendlyLocalizedStringAttribute)attribs[0]).FriendlyKey;
+                foreach (var p in localizer.GetType().GetRuntimeProperties())
+                {
+                    if(p.GetIndexParameters().Length > 0)
+                        return (string)p.GetValue(localizer, new[] { key });                    
+                }
+            }
+
+            return value.ToString();
+        }
+    }
+}
